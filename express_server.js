7f091cf18dca35bfe7bf95f75fa4c6eb505d81ee
Catherine Hu <catherinehu_9898@hotmail.com>
2021-07-05 19:37:24 +0000

    added /hello and morgan

--- express_server.js
+++ express_server.js
@@ -16,6 +16,12 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.use(morgan('dev'));
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
