bcf7957574133129ff09a418143ce6dc2dc6ff2e
Catherine Hu <catherinehu_9898@hotmail.com>
2021-07-05 20:34:19 +0000

    set ejs as view engine

--- express_server.js
+++ express_server.js
@@ -3,10 +3,13 @@ const morgan = require('morgan');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.ca'
 };
+
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -22,6 +25,10 @@ app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars)
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
